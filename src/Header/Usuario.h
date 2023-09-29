7c12a15e2b114de3fa12d3267991c8c169230289
broken_Arm <brunotoro1799@gmail.com>
2023-09-28 22:37:37 -0300

    modificacion

--- src/Header/Usuario.h
+++ src/Header/Usuario.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+class Software;
 using namespace std;
 
 class Usuario{
@@ -20,3 +21,35 @@ class Usuario{
         void agregarSoftware(const Software& software);
         void eliminarSoftware(const Software& software);
 };
+
+Usuario::Usuario(string nombre,int edad) {}
+Usuario::~Usuario() { cout << "Se destruyó objeto usuario" << endl; }
+
+string Usuario::getNombre() {
+    return this->nombre;
+}
+
+int Usuario::getEdad() {
+    return this->edad;
+}
+
+void Usuario::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+void Usuario::setEdad(int edad) {
+    this->edad = edad;
+}
+
+const vector<Software>& Usuario::getBiblioteca() {
+    return biblioteca;
+}
+
+void Usuario::agregarSoftware(const Software& software) {
+    biblioteca.push_back(software);
+}
+
+void Usuario::eliminarSoftware(const Software& software) {
+    // Puedes implementar la lógica para eliminar un software de la biblioteca aquí.
+    // Por ejemplo, puedes buscar el software en la biblioteca y luego eliminarlo.
+}
