9b4200e48c29f2334b2207ac8ef1759dfefe34f6
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 20:08:59 +0000

    asd

---
+++ Clases/Administrador.cpp
@@ -0,0 +1,24 @@
+#include "Administrador.h"
+
+Administrador::Administrador(string nombre,string pasword,int edad,string correo,string AccederLog)
+    :Usuario(nombre,pasword,edad),correo(correo),AccederLog(AccederLog){
+        this->correo=correo;
+        this->AccederLog=AccederLog;
+}
+Administrador::~Administrador(){}
+
+string Administrador::getCorreo(){
+    return this->correo;
+}
+
+string Administrador::getAccederLog(){
+    return this->AccederLog;
+}
+
+void Administrador::setCorreo(string correo){
+    this->correo=correo;
+}
+
+void Administrador::setAccederLog(string AccederLog){
+    this->AccederLog=AccederLog;
+}
