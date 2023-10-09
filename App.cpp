109821cde1fff610bbbc089c1f6db3e77af500a0
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 20:37:08 +0000

    juegos

--- App.cpp
+++ App.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     Sistema sistema;
     sistema.poblarUsuarios();
+    sistema.poblarJuegos();
     // Obtenemos la lista de usuarios
     sistema.imprimirUsuarios();
     return 0;
