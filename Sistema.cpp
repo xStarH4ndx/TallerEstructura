ed77843449dad237c3566d139569c5bc14a24573
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 00:11:18 +0000

    asd

--- Sistema.cpp
+++ Sistema.cpp
@@ -6,7 +6,6 @@ class Sistema{
     public:
         void poblarUsuarios() {
             // Agrega un administrador a la lista de usuarios
-            Administrador admin("Bruno", "123", 22, "cAdmin", "yes");
             cout<<"se agrego admin"<<endl;
         }
 };
