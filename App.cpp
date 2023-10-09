ee7bf80c3c8c01148246134d56c6d593883b1c37
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 03:40:45 +0000

    asd

--- App.cpp
+++ App.cpp
@@ -3,16 +3,11 @@
 #include "Usuario.h"
 #include "Software.h"
 
-void poblarUsuarios();
 
 using namespace std;
 int main() {
-    vector<Usuario>listaUsuarios;
 
     cout<<"iniciando programa"<<endl;
     return 0;
 }
 
-void poblarUsuarios(){
-
-};
