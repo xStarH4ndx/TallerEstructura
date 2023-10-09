61ea8e5b2fab0f60e7f59d929290a3e860ed26df
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 20:09:49 +0000

    asd

---
+++ Clases/Normal.cpp
@@ -0,0 +1,15 @@
+#include "Normal.h"
+
+Normal::Normal(string nombre,string pasword,int edad,string correo)
+    :Usuario(nombre,pasword,edad),correo(correo){
+        this->correo=correo;
+}
+Normal::~Normal(){};
+
+string Normal::getCorreo(){
+    return this->correo;
+}
+
+void Normal::setCorreo(string correo){
+    this->correo=correo;
+}
--- Clases/Normal.h
+++ Clases/Normal.h
@@ -12,17 +12,3 @@ class Normal:public Usuario{
         Normal(string nombre,string pasword,int edad,string correo);
         ~Normal();
 };
-
-Normal::Normal(string nombre,string pasword,int edad,string correo)
-    :Usuario(nombre,pasword,edad),correo(correo){
-        this->correo=correo;
-}
-Normal::~Normal(){};
-
-string Normal::getCorreo(){
-    return this->correo;
-}
-
-void Normal::setCorreo(string correo){
-    this->correo=correo;
-}
