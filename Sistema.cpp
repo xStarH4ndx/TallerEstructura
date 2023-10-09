b707678cd1b41e76fd18305c5909aad7cd7ec811
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 03:13:54 +0000

    asd

--- Sistema.cpp
+++ Sistema.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-//#include "Software.h"
+#include <vector>
 class Usuario;
 
 using namespace std;
 class Sistema{
+    private:
+        vector<Usuario>listaUsuarios;
     public:
         void poblarUsuarios(){
             cout<<"hola"<<endl;
