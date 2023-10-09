007fc3447c1ab8b394e79128165bb1405cf7ea82
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 03:34:18 +0000

    asd

--- Software.h
+++ Software.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
-class Usuario;
+#include "Usuario.h"
 using namespace std;
 
 //ESTA ES LA CLASE PADRE DE TODOS LOS SOFTWARES
@@ -26,47 +26,3 @@ class Software{
         const vector<Usuario>& getListaUsuarios();
         void agregarUsuario(const Usuario& usuario);
 };
-
-Software::Software(string nombre,string developer,int clasificacionEdad,int precio){
-    this->nombre = nombre;
-    this->developer = developer;
-    this->clasificacionEdad = clasificacionEdad;
-    this->precio = precio;
-};
-Software::~Software(){cout<<"Se destruyo objeto Software"<<endl;};
-
-string Software::getNombre(){
-    return this->nombre;
-}
-
-string Software::getDeveloper(){
-    return this->developer;
-}
-
-int Software::getClasificacionEdad(){
-    return this->clasificacionEdad;
-}
-
-int Software::getPrecio(){
-    return this->precio;
-}
-
-void Software::setNombre(string nombre){
-    this-> nombre=nombre;
-}
-
-void Software::setDeveloper(string developer){
-    this->developer=developer;
-}
-
-void Software::setClasificacionEdad(int clasificacionEdad){
-    this->clasificacionEdad=clasificacionEdad;
-}
-
-void Software::setPrecio(int precio){
-    this->precio=precio;
-}
-
-void Software::agregarUsuario(const Usuario& usuario) {
-    listaUsuarios.push_back(usuario);
-}
