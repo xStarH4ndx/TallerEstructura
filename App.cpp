23675f348b2315027e529f934fa8cce9cb006d08
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-10-09 04:11:16 +0000

    asd

--- App.cpp
+++ App.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
-class Usuario;
+#include "Usuario.h"
 
 using namespace std;
 int main() {
     vector<Usuario>listaUsuarios;
-    
+
     cout<<"iniciando programa"<<endl;
     return 0;
 };
